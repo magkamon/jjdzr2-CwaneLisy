bffcaeb97e7e6378832037f959fd02c3f901abd7
Zyta Wiszniewska <zytawisz@gmail.com>
2020-10-31 11:40:23 +0100

    Created a new method to read number from user in Utill class

--- consoleApp/src/main/java/com/infoshare/util/Util.java
+++ consoleApp/src/main/java/com/infoshare/util/Util.java
@@ -1,5 +1,6 @@
 package com.infoshare.util;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Util {
@@ -8,5 +9,25 @@ public class Util {
         System.out.println(message);
         Scanner scanner=new Scanner(System.in);
         return scanner.nextLine();
+    }
+    public static int readNumberFromUser(String message, int upperBound) {
+        boolean numberOutOfRange=true;
+        int readNumberFromUser=0;
+        do {
+            System.out.println(message);
+            try {
+                Scanner scanner = new Scanner(System.in);
+                readNumberFromUser = scanner.nextInt();
+                numberOutOfRange = (readNumberFromUser > upperBound || readNumberFromUser < 1);
+                if(numberOutOfRange){
+                    System.out.println("Nieprawidlowy zakres");
+                }
+            } catch (InputMismatchException ex){
+                numberOutOfRange=true;
+                System.out.println("Niedozwolone znaki");
+            }
+
+        }while(numberOutOfRange);
+        return readNumberFromUser;
     }
 }
