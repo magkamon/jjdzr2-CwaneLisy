2eef2ca683913cdaf987af3bc63ed90334eb9c29
magkamon <kamonmagdalena@gmail.com>
2020-11-16 22:33:05 +0100

    updated after review

--- consoleApp/src/main/java/com/infoshare/service/VolunteerAvailability.java
+++ consoleApp/src/main/java/com/infoshare/service/VolunteerAvailability.java
@@ -1,44 +1,22 @@
 package com.infoshare.service;
 
 import com.infoshare.domain.Volunteer;
-
-import java.util.Scanner;
+import com.infoshare.util.Util;
+import com.infoshare.util.ValidatorEnum;
 
 public class VolunteerAvailability {
 
     private static final String AVAILABILITY_HEADER = "Czy chcesz zmienić swoją dostępność? [ Y / N ] ";
-    private static final String AVAILABILITY_ERROR = "***BŁĄD***";
     private static final String AVAILABILITY_STATUS = "Twoj obecny status w systemie to: ";
     private static final String GET_EMAIL = "Podaj adres e-mail podany podczas rejestracji";
     private static final String STATUS_AVAILABLE = "DOSTĘPNY";
     private static final String STATUS_UNAVAILABLE = "NIEDOSTĘPNY";
 
-    private static boolean askAvailability(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(AVAILABILITY_HEADER);
-        String availability = scanner.nextLine();
-
-        if (availability.equals("Y")){
-            return true;
-        } else if (availability.equals("N")){
-            return false;
-        }
-        else {
-            System.out.println(AVAILABILITY_ERROR);
-            return askAvailability();
-        }
-
-    }
-
     public static void changeAvailability(Volunteer volunteer){
         System.out.println(AVAILABILITY_STATUS);
-        if (volunteer.isAvailable()){
-            System.out.println(STATUS_AVAILABLE);
-        }else {
-            System.out.println(STATUS_UNAVAILABLE);
-        }
-
-        if (VolunteerAvailability.askAvailability()){
+        System.out.println(volunteer.isAvailable() ? STATUS_AVAILABLE : STATUS_UNAVAILABLE);
+        String data = Util.readDataFromConsole(AVAILABILITY_HEADER, ValidatorEnum.YESNO);
+        if (data.equalsIgnoreCase("Y")){
             if(volunteer.isAvailable()){
                 volunteer.setAvailable(false);
             }else {
@@ -47,19 +25,13 @@ public class VolunteerAvailability {
         }
     }
 
-    private static String getEmail (){
-        System.out.println(GET_EMAIL);
-        Scanner sc = new Scanner(System.in);
-        String email = sc.nextLine();
-        return email;
-    }
-
     public static void updateAvailability(){
         // FileDb database = new FileDb();
-        // Volunteer volunteerToUpdate = database.getVolunteer(getEmail);
-        // changeAvailability(volunteerToUpdate);
-        // database.saveVolunteer(volunteerToUpdate);
+//        String email = Util.readDataFromConsole(GET_EMAIL, ValidatorEnum.EMAIL);
+//        Volunteer volunteerToUpdate = database.getVolunteer(email);
+//        if(volunteerToUpdate != null){
+//            changeAvailability(volunteerToUpdate);
+//            database.saveVolunteer(volunteerToUpdate);
+//        }
     }
-
-
 }
