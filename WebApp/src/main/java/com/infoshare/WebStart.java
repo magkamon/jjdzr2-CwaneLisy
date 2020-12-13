0de28e68f975f308361834702294f301e0133937
magkamon <kamonmagdalena@gmail.com>
2020-12-13 12:46:09 +0100

    JJDZR2CL-15 done

---
+++ WebApp/src/main/java/com/infoshare/WebStart.java
@@ -0,0 +1,12 @@
+package com.infoshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStart {
+    public static void main(String[] args) {
+        SpringApplication.run(WebStart.class,args);
+    }
+
+}
