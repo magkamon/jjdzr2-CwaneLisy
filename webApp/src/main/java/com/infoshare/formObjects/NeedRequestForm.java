b66ec88c332e18f91a05b058821ca7c149fce943
zyta <zytawisz@gmail.com>
2021-01-01 13:58:47 +0100

    Part of JJDZR2CL-20 task

---
+++ webApp/src/main/java/com/infoshare/formObjects/NeedRequestForm.java
@@ -0,0 +1,45 @@
+package com.infoshare.formObjects;
+
+import com.infoshare.domain.TypeOfHelp;
+
+public class NeedRequestForm {
+    private String name;
+    private String location;
+    private String phone;
+    private TypeOfHelp typeOfHelp;
+
+    public NeedRequestForm() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public TypeOfHelp getTypeOfHelp() {
+        return typeOfHelp;
+    }
+
+    public void setTypeOfHelp(TypeOfHelp typeOfHelp) {
+        this.typeOfHelp = typeOfHelp;
+    }
+}
