e2b62be5fb04f5b93794779edfe8cc5a52145188
pyrek321 <aleks.swinoga@wp.pl>
2021-01-12 19:51:23 +0100

    WebInterface 90%done

--- webApp/src/main/java/com/infoshare/controller/NeedRequestController.java
+++ webApp/src/main/java/com/infoshare/controller/NeedRequestController.java
@@ -9,46 +9,46 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("need-request")
-public class NeedRequestController {
-    private final NeedRequestService needRequestService;
+    public class NeedRequestController {
+        private final NeedRequestService needRequestService;
 
-    @Autowired
-    public NeedRequestController(NeedRequestService needRequestService) {
-        this.needRequestService = needRequestService;
-    }
+        @Autowired
+        public NeedRequestController(NeedRequestService needRequestService) {
+            this.needRequestService = needRequestService;
+        }
 
-    @GetMapping("/create")
-    public String createNeedRequest(Model model) {
-        return getTestViewWithPageUnderConstructionMessage(model);
-    }
+        @GetMapping("/create")
+        public String createNeedRequest(Model model) {
+            return getTestViewWithPageUnderConstructionMessage(model);
+        }
 
-    @GetMapping("/search")
-    public String searchForNeedRequest(Model model) {
-        return getTestViewWithPageUnderConstructionMessage(model);
-    }
+        @GetMapping("/search")
+        public String searchForNeedRequest(Model model) {
+            return getTestViewWithPageUnderConstructionMessage(model);
+        }
 
-    @GetMapping("/edit")
-    public String editNeedRequest(Model model) {
-        return getTestViewWithPageUnderConstructionMessage(model);
-    }
+        @GetMapping("/edit")
+        public String editNeedRequest(Model model) {
+            return getTestViewWithPageUnderConstructionMessage(model);
+        }
 
-    @GetMapping("/associate-to-volunteer")
-    public String associateNeedRequestToVolunteer(Model model) {
-        return getTestViewWithPageUnderConstructionMessage(model);
-    }
+        @GetMapping("/associate-to-volunteer")
+        public String associateNeedRequestToVolunteer(Model model) {
+            return getTestViewWithPageUnderConstructionMessage(model);
+        }
 
-    @GetMapping("/add-comment")
-    public String addCommentToNeedRequest(Model model) {
-        return getTestViewWithPageUnderConstructionMessage(model);
-    }
+        @GetMapping("/add-comment")
+        public String addCommentToNeedRequest(Model model) {
+            return getTestViewWithPageUnderConstructionMessage(model);
+        }
 
-    @GetMapping("/browse-history")
-    public String browseHistoryOfNeedRequest(Model model) {
-        return getTestViewWithPageUnderConstructionMessage(model);
-    }
+        @GetMapping("/browse-history")
+        public String browseHistoryOfNeedRequest(Model model) {
+            return getTestViewWithPageUnderConstructionMessage(model);
+        }
 
-    private String getTestViewWithPageUnderConstructionMessage(Model model) {
-        model.addAttribute("message", "This page is under construction...");
-        return "test-view";
-    }
+        private String getTestViewWithPageUnderConstructionMessage(Model model) {
+            model.addAttribute("message", "This page is under construction...");
+            return "test-view";
+        }
 }
